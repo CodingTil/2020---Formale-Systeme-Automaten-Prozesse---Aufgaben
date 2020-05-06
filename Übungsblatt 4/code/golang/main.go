f0e7abb37d94dff2f3b4fcb3edcb754502d1f5e4
CodingTil <36734749+CodingTil@users.noreply.github.com>
2020-05-06 16:17:02 +0200

    Index war falsch

--- Übungsblatt 4/code/golang/main.go
+++ Übungsblatt 4/code/golang/main.go
@@ -12,7 +12,7 @@ var t [20]time.Duration
 func main() {
 	pattern := "(.*?,){13}z"
 	r,_ := regexp.Compile(pattern)
-	for i := 0; i < 20; i++ {
+	for i := 1; i <= 20; i++ {
 		go func(i int) {
 			w := "1,2,3,4,5,6,7,8,9,10,11,12"
 			for j:= 0; j < i; j++ {
@@ -22,25 +22,25 @@ func main() {
 			result := r.MatchString(w)
 			tm := time.Now()
 			elapsed := tm.Sub(start)
-			results[i] = result
-			t[i] = elapsed
+			results[i-1] = result
+			t[i-1] = elapsed
 		}(i)
 	}
 
 	var avg int64
 	avg = 0;
 	fmt.Println("Index | Result | Duration")
-	for i:= 0; i < 20; i++ {
+	for i:= 1; i <= 20; i++ {
 		fmt.Printf("%v    ",i)
 		if i < 10 {
 			fmt.Printf(" ")
 		}
-		fmt.Printf("| %v  ", results[i])
-		if results[i] == true {
+		fmt.Printf("| %v  ", results[i-1])
+		if results[i-1] == true {
 			fmt.Printf(" ")
 		}
-		fmt.Printf("| %v\n", t[i])
-		avg += t[i].Microseconds()
+		fmt.Printf("| %v\n", t[i-1])
+		avg += t[i-1].Microseconds()
 	}
 
 	avg /= 20
--- Übungsblatt 4/code/java/Main.java
+++ Übungsblatt 4/code/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         long[] time = new long[20];
         
         pattern = Pattern.compile(patternString);
-        for(int i = 0; i < 20; i++) {
+        for(int i = 1; i <= 20; i++) {
             String text = "1,2,3,4,5,6,7,8,9,10,11,12";
             for(int j = 0; j < i; j++) {
                 text += ",1";
@@ -19,20 +19,20 @@ public class Main {
             boolean result = pattern.matcher(text).matches();
             long end = System.nanoTime();
             long elapsed = (end - start) / 1000000;
-            results[i] = result;
-            time[i] = elapsed;
+            results[i-1] = result;
+            time[i-1] = elapsed;
         }
 
         long avg = 0;
         System.out.println("Index | Result | Duration (ms)");
-        for(int i = 0; i < 20; i++) {
+        for(int i = 01; i <= 20; i++) {
             String text = i + "    ";
             if(i < 10) text += " ";
-            text += "| " + results[i] + "  ";
-            if(results[i] == true) text += " ";
-            text += "| " + time[i];
+            text += "| " + results[i-1] + "  ";
+            if(results[i-1] == true) text += " ";
+            text += "| " + time[i-1];
             System.out.println(text);
-            avg += time[i];
+            avg += time[i-1];
         }
 
         avg /= 20;
