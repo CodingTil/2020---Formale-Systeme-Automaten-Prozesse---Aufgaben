a99bc780cfbdf7f589e4a87d2b6c673ef979aec7
CodingTil <36734749+CodingTil@users.noreply.github.com>
2020-06-18 17:58:55 +0200

    AB->BC corrected

--- Übungsblatt 8/Bool Eval/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"BracketMismatchException"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"SyntaxErrorException"},{"p":"<Unnamed>","l":"UnknownSymbolException"}]
